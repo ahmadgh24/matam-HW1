f3476158eee1c21060cdf892e4449f8564da5b84
Ahmad Ghanayim <aganaym50@outlook.com>
2024-07-01 19:04:39 +0300

    added tests-2

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,6 +1,6 @@
-macro(install_project_binaries target)
-    install(TARGETS ${target}
-            DESTINATION bin
-            COMPONENT ${target}
-    )
-endmacro()
+cmake_minimum_required(VERSION 3.28)
+project(1)
+
+set(CMAKE_CXX_STANDARD 17)
+
+add_executable(1 main.cpp Transaction.cpp Transaction.h Blockchain.h Blockchain.cpp Utilities.h Utilities.cpp)
